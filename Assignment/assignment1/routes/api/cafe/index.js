c3d0b996d79e685c5a474b093086981020fa5f1f
ChoSooMin <vywlgns1102@naver.com>
2019-11-20 03:17:57 +0900

    assignment1 modify file system

--- Assignment/assignment1/routes/api/cafe/index.js
+++ Assignment/assignment1/routes/api/cafe/index.js
@@ -1,10 +1,9 @@
-// hw > routes > api > blog.js
-
+// assignment1 > routes > api > cafe > index.js
 var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('localhost:3000/api/blog');
+    res.send('localhost:3000/api/cafe');
 });
 
 module.exports = router;
--- Assignment/assignment1/routes/api/index.js
+++ Assignment/assignment1/routes/api/index.js
@@ -8,8 +8,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express /api' });
 });
 
-router.use('/news', require('./news')); // localhost:3000/api/news 설정
 router.use('/cafe', require('./cafe')); // localhost:3000/api/cafe 설정
 router.use('/blog', require('./blog')); // localhost:3000/api/blog 설정
+router.use('/news', require('./news')); // localhost:3000/api/news 설정
 
 module.exports = router;
