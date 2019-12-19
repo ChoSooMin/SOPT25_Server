7d76b16a67332b070280c38f0e06439dc6131c2a
ChoSooMin <vywlgns1102@naver.com>
2019-12-20 01:16:13 +0900

    repackaging for Level2

--- Assignment/assignment3/routes/blogs/index.js
+++ Assignment/assignment3/routes/blogs/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// router.use('/:blogIdx/articles', require('./articles'));
+router.use('/:blogIdx/articles', require('./articles'));
 router.use('/', require('./blog'));
 
 module.exports = router;
